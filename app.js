91212869b35530606f514e27973d05acdc6ec497
Brawn Idah <idabrawn45@gmail.com>
2023-10-13 13:15:35 +0300

    added a function to display error message to the user

--- app.js
+++ app.js
@@ -16,3 +16,16 @@ pauseButton.addEventListener('click', () => {
         successMessage.style.display = 'block'; // Show the success message
     }
 })
+
+.then(data => {
+    if (data.success) {
+        // The to-do task was successfully added
+        const successMessage = document.getElementById('success-message');
+        successMessage.style.display = 'block'; // Show the success message
+
+        // Hide the success message after 3 seconds
+        setTimeout(() => {
+            successMessage.style.display = 'none';
+        }, 3000); // 3000 milliseconds (3 seconds)
+    }
+})
